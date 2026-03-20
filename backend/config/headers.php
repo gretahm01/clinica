0db523e1bcc1b065b67ed42a8bc7b70bdb437f39
Greta <greta0401hm@gmail.com>
2026-03-20 00:07:40 -0600

    merge de cami a greta y backend PHP login con JWT - en progreso
    juntamos back con front y luego intentamos ingresar con login con una credencial pero no se pudo

--- backend/config/headers.php
+++ backend/config/headers.php
@@ -1,6 +1,6 @@
 <?php
-// Permite que React (puerto 5173) se conecte al back
-header("Access-Control-Allow-Origin: http://localhost:5173");
+// Permite que React (puerto 5174) se conecte al back
+header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
