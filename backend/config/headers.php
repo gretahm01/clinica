5993b9c30e656462199f6505c8e5f9960db894d2
Greta <greta0401hm@gmail.com>
2026-03-22 23:09:36 -0600

    comentarios db y headers
    estudié los dos archivos de db.php y headers.php y añadí comentarios

--- backend/config/headers.php
+++ backend/config/headers.php
@@ -1,4 +1,5 @@
 <?php
+//se establece un origen, se permite cors 
 $origen = $_SERVER['HTTP_ORIGIN'] ?? '*';
 
 header("Access-Control-Allow-Origin: $origen");
@@ -7,6 +8,8 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
 
+//cuando se hace options se responde con 200
+//se hace esto para que el navegador sepa que puede hacer la petición real después del preflight
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit();
