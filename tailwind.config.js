7d4ec5421d0466110b217e45f48c6b21f08cb965
Greta <greta0401hm@gmail.com>
2026-03-25 11:38:20 -0600

    pacientes ya conecto al backend pero error de conexión al GET /pacientes.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: "#A3BFA8",       // verde principal
-        "primary-hover": "#7FA889", // verde hover
-        background: "#ECEFED",    // fondo
-        dark: "#3e5747",          // texto oscuro
-      }
-    },
-  },
-  plugins: [],
-=======
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -28,5 +12,4 @@ export default {
     },
   },
   plugins: [],
->>>>>>> origin/FrontCami
 }
